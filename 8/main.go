164fe63728275ccf795ad32279c7e08747f807f8
Rossiar <rosshenderson92@gmail.com>
2023-12-14 16:03:16 -0500

    day 8 task 2
    have to admit the LCM/GCD stuff was beyond me and I would never have even had the language to describe what I wanted (multiplying all the endings together was my best guess)

--- 8/main.go
+++ 8/main.go
@@ -3,9 +3,11 @@ package main
 import (
 	"log"
 	"os"
+	"slices"
 	"strings"
 
 	aoc "github.com/rossiar/advent2023"
+	"golang.org/x/exp/maps"
 )
 
 func main() {
@@ -14,7 +16,7 @@ func main() {
 	if err != nil {
 		panic(err.Error())
 	}
-	if err := Task1(lines); err != nil {
+	if err := Task2(lines); err != nil {
 		panic(err.Error())
 	}
 }
@@ -61,4 +63,84 @@ func Task1(lines []string) error {
 	}
 	log.Println(steps)
 	return nil
+}
+
+func Task2(lines []string) error {
+	directions := lines[0]
+	nodes := make(map[string]node, 0)
+	refs := make([]node, 0)
+	for i := 2; i < len(lines); i++ {
+		lineParts := strings.Split(lines[i], "=")
+		id := strings.TrimSpace(lineParts[0])
+		nodeParts := strings.Split(lineParts[1], ",")
+		n := node{
+			ID:    id,
+			Left:  nodeParts[0][2:],
+			Right: nodeParts[1][1:4],
+		}
+		nodes[id] = n
+		if strings.HasSuffix(id, "A") {
+			refs = append(refs, n)
+		}
+	}
+	steps := 0
+	d := 0
+	endings := make(map[string]int, 0)
+	for {
+		end := false
+		for i, current := range refs {
+			_, found := endings[current.ID]
+			if strings.HasSuffix(current.ID, "Z") && !found {
+				endings[current.ID] = steps
+				log.Printf("found ending %s at step %d", current.ID, steps)
+				if len(refs) == 1 {
+					end = true
+				}
+				refs = append(refs[:i], refs[i+1:]...)
+			}
+		}
+		if end {
+			log.Println(endings)
+			break
+		}
+		direction := directions[d]
+		steps++
+		for i, current := range refs {
+			next := current.Right
+			if direction == 'L' {
+				next = current.Left
+			}
+			//log.Printf("going %c from %s to %s", direction, current.ID, next)
+			refs[i] = nodes[next]
+		}
+		d++
+		if d == len(directions) {
+			d = 0
+		}
+	}
+	vals := maps.Values(endings)
+	slices.Sort(vals)
+	log.Println(LCM(vals[0], vals[1], vals[1:]...))
+	return nil
+}
+
+// Greatest Common Divisor
+func GCD(a, b int) int {
+	for b != 0 {
+		tmp := b
+		b = a % b
+		a = tmp
+	}
+	return a
+}
+
+// Lowest Common Multiple
+func LCM(a, b int, integers ...int) int {
+	result := a * b / GCD(a, b)
+
+	for i := 0; i < len(integers); i++ {
+		result = LCM(result, integers[i])
+	}
+
+	return result
 }
