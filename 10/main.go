09005d59749c7b3886929a65cec6e2bf0012b95c
Rossiar <rosshenderson92@gmail.com>
2023-12-15 12:13:59 -0500

    day 10 task 2
    this breaks with example 3 and to be honest I copied the code from here (https://github.com/ianmihura/advent23/blob/master/day_10/day_10.go) because the maths is too much for me

--- 10/main.go
+++ 10/main.go
@@ -17,7 +17,7 @@ func main() {
 	if err != nil {
 		panic(err.Error())
 	}
-	if err := Task1(lines); err != nil {
+	if err := Task2(lines); err != nil {
 		panic(err.Error())
 	}
 }
@@ -58,103 +58,127 @@ func (p point) Transform(dir direction) point {
 // F is a 90-degree bend connecting south and east.
 // . is ground; there is no pipe in this tile.
 func Task1(lines []string) error {
-	var startX, startY int
-	for y, line := range lines {
-		if x := strings.Index(line, "S"); x != -1 {
-			startX = x
-			startY = y
-			break
-		}
+	path, err := start(lines)
+	if err != nil {
+		return err
 	}
-	log.Println(start(lines, startX, startY))
+	log.Println(len(path) / 2)
 	return nil
 }
 
-func start(lines []string, x, y int) int {
-	start := point{x, y}
+func start(lines []string) ([]point, error) {
+	start := point{}
+	for y, line := range lines {
+		if x := strings.Index(line, "S"); x != -1 {
+			start.X = x
+			start.Y = y
+			break
+		}
+	}
+	path := []point{start}
 	log.Println("looking north from S")
-	nSteps, err := follow(lines, North, start.Transform(North), 1)
+	nSteps, err := traverse(lines, North, start.Transform(North), path)
 	if err == nil {
-		return nSteps / 2
+		return nSteps, nil
 	}
 	log.Println("looking east from S")
-	eSteps, err := follow(lines, East, start.Transform(East), 1)
+	eSteps, err := traverse(lines, East, start.Transform(East), path)
 	if err == nil {
-		return eSteps / 2
+		return eSteps, nil
 	}
 	log.Println("looking south from S")
-	sSteps, err := follow(lines, South, start.Transform(South), 1)
+	sSteps, err := traverse(lines, South, start.Transform(South), path)
 	if err == nil {
-		return sSteps / 2
+		return sSteps, nil
 	}
 	log.Println("looking west from S")
-	wSteps, err := follow(lines, West, start.Transform(West), 1)
+	wSteps, err := traverse(lines, West, start.Transform(West), path)
 	if err == nil {
-		return wSteps / 2
+		return wSteps, nil
 	}
-	return -1
+	return nil, fmt.Errorf("no paths from %+v", start)
 }
 
 var ErrDeadEnd = errors.New("dead end")
 
-func follow(grid []string, dir direction, p point, steps int) (int, error) {
+func traverse(grid []string, dir direction, p point, path []point) ([]point, error) {
 	if p.Y < 0 && dir == North {
 		log.Printf("dead end going %s at %+v", dir, p)
-		return -1, ErrDeadEnd
+		return nil, ErrDeadEnd
 	}
 	if p.X < 0 && dir == West {
 		log.Printf("dead end going %s at %+v", dir, p)
-		return -1, ErrDeadEnd
+		return nil, ErrDeadEnd
 	}
 	if p.Y > len(grid)-1 && dir == South {
 		log.Printf("dead end going %s at %+v", dir, p)
-		return -1, ErrDeadEnd
+		return nil, ErrDeadEnd
 	}
 	if p.X > len(grid[p.Y])-1 && dir == East {
 		log.Printf("dead end going %s at %+v", dir, p)
-		return -1, ErrDeadEnd
+		return nil, ErrDeadEnd
 	}
 	//log.Printf("going %s to %+v", dir, p)
 	switch grid[p.Y][p.X] {
 	case 'S':
-		return steps, nil
+		return path, nil
 	case '|':
 		if dir == North || dir == South {
-			return follow(grid, dir, p.Transform(dir), steps+1)
+			return traverse(grid, dir, p.Transform(dir), append(path, p))
 		}
 	case '-':
 		if dir == East || dir == West {
-			return follow(grid, dir, p.Transform(dir), steps+1)
+			return traverse(grid, dir, p.Transform(dir), append(path, p))
 		}
 	case 'L':
 		if dir == South {
-			return follow(grid, East, p.Transform(East), steps+1)
+			return traverse(grid, East, p.Transform(East), append(path, p))
 		} else if dir == West {
-			return follow(grid, North, p.Transform(North), steps+1)
+			return traverse(grid, North, p.Transform(North), append(path, p))
 		}
 	case 'J':
 		if dir == East {
-			return follow(grid, North, p.Transform(North), steps+1)
+			return traverse(grid, North, p.Transform(North), append(path, p))
 		} else if dir == South {
-			return follow(grid, West, p.Transform(West), steps+1)
+			return traverse(grid, West, p.Transform(West), append(path, p))
 		}
 	case '7':
 		if dir == North {
-			return follow(grid, West, p.Transform(West), steps+1)
+			return traverse(grid, West, p.Transform(West), append(path, p))
 		} else if dir == East {
-			return follow(grid, South, p.Transform(South), steps+1)
+			return traverse(grid, South, p.Transform(South), append(path, p))
 		}
 	case 'F':
 		if dir == North {
-			return follow(grid, East, p.Transform(East), steps+1)
+			return traverse(grid, East, p.Transform(East), append(path, p))
 		} else if dir == West {
-			return follow(grid, South, p.Transform(South), steps+1)
+			return traverse(grid, South, p.Transform(South), append(path, p))
 		}
 	}
 	log.Printf("dead end at %c going %s at %+v", grid[p.Y][p.X], dir, p)
-	return -1, ErrDeadEnd
+	return nil, ErrDeadEnd
 }
 
 func Task2(lines []string) error {
+	path, err := start(lines)
+	if err != nil {
+		return err
+	}
+	log.Println(len(path))
+
+	// shoelace algorithm
+	trailing := 0
+	for i := 0; i < len(path); i++ {
+		if i == len(path)-1 {
+			trailing += (path[i].Y + path[0].Y) * (path[i].X - path[0].X)
+		} else {
+			trailing += (path[i].Y + path[i+1].Y) * (path[i].X - path[i+1].X)
+		}
+	}
+	area := trailing / 2
+
+	// Pick's theorem
+	inner := area - (len(path) / 2) + 1
+	log.Println(inner)
 	return nil
 }
